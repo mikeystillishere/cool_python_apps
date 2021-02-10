3192f2970630d8161627808f9dad672c9ee7ee30
mikeystillishere <78045465+mikeystillishere@users.noreply.github.com>
2021-02-10 13:09:53 -0600

    Update instagram.py

--- 3_instagram/instagram.py
+++ 3_instagram/instagram.py
@@ -1,6 +1,6 @@
 from instabot import Bot
 bot = Bot()
-bot.login(username="", password="")
+bot.login(username="mikeystillishere", password="malagros0810")
 
 ######  upload a picture #######
 bot.upload_photo("yoda.jpg", caption="biscuit eating baby")
